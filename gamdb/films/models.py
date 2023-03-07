c8c9e827ba8106f2751ddb283a709a4a79225e51
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-03-07 11:02:36 +0100

    last lesson code not broken

--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 class Movie(models.Model):
-        name = models.CharField()
+        name = models.CharField(max_length=200)
         year = models.IntegerField()
         descriptiom = models.TextField()
