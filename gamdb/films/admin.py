b2f02c904d3fb7a9e5177df9d60d86d5048d3d32
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-03-21 11:20:32 +0100

    models, admin polechtání

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Director
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'director', 'year', 'genres_display']
+    list_display_links = ['name']
+    search_fields = ['name']
+
 admin.site.register(Movie)
+admin.site.register(Director)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -2,5 +2,24 @@ from django.db import models
 
 class Movie(models.Model):
         name = models.CharField(max_length=200)
-        year = models.IntegerField()
+        year = models.IntegerField(blank=True)
         descriptiom = models.TextField()
+        director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+        genres = models.ManyToManyField('Genre', blank=True, null=True)
+
+        def __str__(self):
+            return f'{self.name} {self.year}'
+        def genres_display(self):
+               [i.name for i in self.genres.all]
+
+class Director(models.Model):
+        name = models.CharField(max_length=200)
+        birth_year = models.IntegerField()
+        def __str__(self):
+               return f'{self.name} ({self.birth_year})'
+        
+class Genre(models.Model):
+       name = models.CharField(max_length=255)
+
+       def __str__(self):
+              return f'{self.name}'
